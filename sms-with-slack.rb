5ffdbbdab02fce5cd6b38559c0a1bb52e6c7c4f9
Tiago Pereira <wildlifechorus@users.noreply.github.com>
2016-01-05 15:11:35 +0000

    Update sms-with-slack.rb

--- sms-with-slack.rb
+++ sms-with-slack.rb
@@ -15,58 +15,58 @@ end
 TWILIO_NUMBER		= "+15005550006"
 TWILIO_ACCOUNT_SID	= "AC712b324e706b43931df20c86728f3ddc5"
 TWILIO_AUTH_TOKEN	= "f4884f79955a2dd4bea48994964404a8a"
-@twilio 			= Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+@twilio             = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
 # Init Slack Client
 SLACK_CLIENT = Slack::RealTime::Client.new
 SLACK_CLIENT.on :hello do
-	log_this("Connected! Start sending SMS from " + TWILIO_NUMBER + "!")
+    log_this("Connected! Start sending SMS from " + TWILIO_NUMBER + "!")
 end
 
 # Logger
 def log_this(msg)
-	puts "#{Time.now}: #{msg.tr('*', '')}"
+    puts "#{Time.now}: #{msg.tr('*', '')}"
 end
 
 # SMS Method!
 def sms(number, message)
-	@twilio.messages.create(
-		from:   TWILIO_NUMBER, 
-		to:     number, 
-		body:   message
-	)
-	log_this("SMS sent to #{number} - #{message}")
+    @twilio.messages.create(
+        from:   TWILIO_NUMBER, 
+        to:     number, 
+        body:   message
+    )
+    log_this("SMS sent to #{number} - #{message}")
 end
 
 # Message on Slack
 def msgSlack(channel, message)
-	SLACK_CLIENT.message channel: channel, text: message
+    SLACK_CLIENT.message channel: channel, text: message
 end
 
 # Watch messages on Slack and react!
 SLACK_CLIENT.on :message do |data|
-
-	# Take care of data
-	content			= data['text'].split(" ", 3)
-	number			= content[1].nil? ? false : content[1]
-	message			= content[2].nil? ? false : content.last
-
-	# Commands
-	case content.first
-		when "SMS" then 
-			unless Phonelib.valid?(number) and message
-				msgSlack(data['channel'], "*Something went wrong, check the number and message...*")
-				log_this("Error sending message (#{message}) to #{number} from #{data['user']}!")
-			else
-				sms(number, message)
-				msgSlack(data['channel'], "*Message sent to #{number} - #{message}*")			
-				log_this("Message (#{message}) sent to #{number} from #{data['user']}!")
-			end
-		when "HELP" then
-			msgSlack(data['channel'], "*Usage: SMS +351917723456 Message you want to send*")
-		when /^bot/ then
-			msgSlack(data['channel'], "*What?, type 'HELP'!*")
-	end	
+    
+    # Take care of data
+    content			= data['text'].split(" ", 3)
+    number			= content[1].nil? ? false : content[1]
+    message			= content[2].nil? ? false : content.last
+    
+    # Commands
+    case content.first
+        when "SMS" then 
+            unless Phonelib.valid?(number) and message
+                msgSlack(data['channel'], "*Something went wrong, check the number and message...*")
+                log_this("Error sending message (#{message}) to #{number} from #{data['user']}!")
+            else
+                sms(number, message)
+                msgSlack(data['channel'], "*Message sent to #{number} - #{message}*")			
+                log_this("Message (#{message}) sent to #{number} from #{data['user']}!")
+            end
+        when "HELP" then
+            msgSlack(data['channel'], "*Usage: SMS +351917723456 Message you want to send*")
+        when /^bot/ then
+            msgSlack(data['channel'], "*What?, type 'HELP'!*")
+    end
 end
 
 SLACK_CLIENT.start!
